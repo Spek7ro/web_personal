46a91c71e4c2e004bd54501eb8e303fcbe875f80
cristian <20201032@uaz.edu.mx>
2024-10-28 21:59:13 -0600

    Tailwind config, fonts, globals.css

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,16 @@
-import localFont from "next/font/local";
+import {JetBrains_Mono} from '@next/font/google';
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+// Components  
+import Header from '@/components/Header';
+
+
+const jetbrainsMono = JetBrains_Mono({ 
+    subsets: ['latin'], 
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+    variable: '--font-jetbrainsMono',
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata = {
   title: "Create Next App",
@@ -21,8 +21,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={jetbrainsMono.variable}>
+        <Header/>
         {children}
       </body>
     </html>
