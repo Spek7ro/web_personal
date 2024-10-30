1715bac3d414c469bf87bb67440a5d62005783f7
cristian <20201032@uaz.edu.mx>
2024-10-29 22:16:31 -0600

    Framer Motion Transitions

---
+++ components/Stairs.jsx
@@ -0,0 +1,27 @@
+import { motion } from "framer-motion";
+import { init } from "next/dist/compiled/webpack/webpack";
+
+// Variants
+const stairAnimation = {
+  initial: { 
+    top: "0%",
+  },
+  animate: {
+    top: "100%",
+  },
+  exit: {
+    top: ["100%", "0%"],
+  },
+};
+
+const Stairs = () => {
+  return <>
+    {/* render 6 motion.divs, each representing a step of the stairs 
+      Each div will have the same animatio defined by the stairsAnimation object
+      The delay for each div is calculated dynamically based on it's reversed index
+      Crea
+    */}
+  </>;
+};
+
+export default Stairs;
