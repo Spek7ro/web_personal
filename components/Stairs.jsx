fcbe9b746bea6fa8e53771c06b4e04cf9f67cbc5
cristian <20201032@uaz.edu.mx>
2024-11-07 21:53:15 -0600

    Add StairTransition

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -1,5 +1,7 @@
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 // import { init } from "next/dist/compiled/webpack/webpack";
+
+import { MotionConfig } from "framer-motion";
 
 // Variants
 const stairAnimation = {
@@ -14,14 +16,35 @@ const stairAnimation = {
   },
 };
 
+const reverseIndex = (index) => {
+  const totalSteps = 6; // Number of steps
+  return totalSteps - index; // Calculate the reversed index
+};
+
 const Stairs = () => {
   return (
     <>
       {/* render 6 motion.divs, each representing a step of the stairs 
       Each div will have the same animatio defined by the stairsAnimation object
       The delay for each div is calculated dynamically based on it's reversed index
-      Crea
     */}
+      {[...Array(6)].map((_, index) => {
+        return (
+          <motion.div
+            key={index}
+            variants={stairAnimation}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              duration: 0.4,
+              ease: "easeInOut",
+              delay: reverseIndex(index) * 0.1,
+            }}
+            className="h-full w-full bg-white relative "
+          />
+        );
+      })}
     </>
   );
 };
