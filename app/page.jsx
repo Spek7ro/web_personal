0fb83194f95c2fc047070a338d3ddfa837d93cdb
Cristian <20201032@uaz.edu.mx>
2024-11-13 22:02:09 -0600

    add HomePage

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,47 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+// Components
+import Social from "@/components/Social";
+
+const Home = () => {
   return (
-    <main>
-      <h1>Home Page</h1>
-    </main>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24 ">
+          {/*  text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Engineer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br />{" "}
+              <span className="text-accent">Cristian Alvarado</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              Software engineer passionate about technology and cybersecurity.
+              Focused on developing innovative solutions and exploring the
+              intersection of programming and security
+            </p>
+            {/* btn and socials */}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text_xl " />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social />
+              </div>
+            </div>
+          </div>
+          {/*  photo */}
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
   );
-}
+};
+
+export default Home;
---
+++ components/Social.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
+
+
+const Social = () => {
+  return (
+    <div>
+      socials
+    </div>
+  );
+};
+
+export default Social;
