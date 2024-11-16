2b067be3dfdd5df82a1b2bbddd4dbfbda9ab197c
Cristian <20201032@uaz.edu.mx>
2024-11-15 22:46:49 -0600

    add photo and socials in the HomePage

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import { FiDownload } from "react-icons/fi";
 
 // Components
 import Social from "@/components/Social";
+import Photo from "@/components/Photo";
 
 const Home = () => {
   return (
@@ -10,10 +11,10 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24 ">
           {/*  text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Engineer</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br />{" "}
+              Hello I'm <br />{" "} 
               <span className="text-accent">Cristian Alvarado</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
@@ -32,12 +33,18 @@ const Home = () => {
                 <FiDownload className="text_xl " />
               </Button>
               <div className="mb-8 xl:mb-0">
-                <Social />
+                <Social
+                  containerStyle="flex gap-6"
+                  iconStyle="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent 
+                  text-base hover:bg-accent hover:text-primary hover_trasition-all duration-500"
+                />
               </div>
             </div>
           </div>
           {/*  photo */}
-          <div>photo</div>
+          <div>
+            <Photo className="order-1 xl:order-none mb-8 xl:mb-0" /> 
+          </div>
         </div>
       </div>
     </section>
--- components/Social.jsx
+++ components/Social.jsx
@@ -1,11 +1,20 @@
 import Link from "next/link";
 import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
 
+const sociales = [
+  { icon: <FaGithub />, path: "https://github.com/Spek7ro" },
+  { icon: <FaLinkedin />, path: "#" },
+  { icon: <FaTwitter />, path: "#" },
+];
 
-const Social = () => {
+const Social = ({ containerStyle, iconStyle }) => {
   return (
-    <div>
-      socials
+    <div className={containerStyle}>
+      {sociales.map((item, index) => {
+        return <Link key={index} href={item.path} className={iconStyle}>
+          {item.icon}
+        </Link>;
+      })}
     </div>
   );
 };
