fadc8c9ca27864a2e40ad6c2f90a036e9cc8752e
Cristian <20201032@uaz.edu.mx>
2025-01-14 21:37:21 -0600

    add resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,25 @@
+"use client";
+
+import {
+  FaJava,
+  FaPython,
+  FaNodeJs,
+  FaDocker,
+} from "react-icons/fa";
+
+import { GrOracle, GrDatabase } from "react-icons/gr";
+import { SiMysql } from "react-icons/si";
+import { BiLogoSpringBoot } from "react-icons/bi";
+import { FaGit } from "react-icons/fa";
+
+// about me
+const about = {
+  title: "About Me",
+  description:""
+
+}
+    
+
 const Resume = () => {
   return (
     <main>
