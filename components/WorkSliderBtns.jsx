cfe4c0c438b82928688de4d0aaaefe6fcaaff43b
Cristian <chalaca23ff@gmail.com>
2025-01-27 11:41:40 -0600

    add WorkSliderBtns

--- components/WorkSliderBtns.jsx
+++ components/WorkSliderBtns.jsx
@@ -6,12 +6,12 @@ import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
 const WorkSliderBtns = (containerStyles, btnStyles, iconsStyles) => {
   const swiper = useSwiper();
   return (
-    <div className="containerStyles">
-      <button className="btnStyles" onClick={() => swiper.slidePrev()}>
-        <PiCaretLeftBold className="iconsStyles"/>
+    <div className={containerStyles}>
+      <button className={btnStyles} onClick={() => swiper.slidePrev()}>
+        <PiCaretLeftBold className={iconsStyles} />
       </button>
-      <button className="btnStyles" onClick={() => swiper.slideNext()}>
-        <PiCaretRightBold className="iconsStyles"/>
+      <button className={btnStyles} onClick={() => swiper.slideNext()}>
+        <PiCaretRightBold className={iconsStyles} />
       </button>
     </div>
   );
