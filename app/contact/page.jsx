6c683ef7fda7ac95de673095723730f5d3376638
Cristian <chalaca23ff@gmail.com>
2025-01-28 23:13:40 -0600

    add contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,8 +1,70 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+import { motion } from "framer-motion";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "(+52) 492 223 1030",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "cristianrdz75453@gmail.com",
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Address",
+    description: "Zacatecas, Mexico",
+  },
+];
+
 const Contact = () => {
   return (
-    <main>
-      <h1>Contact Page</h1>
-    </main>
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/* form */}
+          <div className="xl:h-[54%] order-2 xl:order-none">
+            <form
+              className="flex flex-col gap-6 p-10 bg-[#27272c] 
+            rounded-xl"
+            >
+              <h3>Let's work together</h3>
+            </form>
+          </div>
+          {/* info */}
+          <div
+            className="flex-1 flex items-center xl:justify-end order-1 
+            xl:order-none mb-8 xl:mb-0"
+          >
+            info
+          </div>
+        </div>
+      </div>
+    </motion.section>
   );
 };
 
