90f9cc65771cbc6d88cb170e040d7d24458cc1be
Cristian <71907925+Spek7ro@users.noreply.github.com>
2025-11-06 22:44:06 -0600

    add link Google Cloud Plataform

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
-import { FaLinkedin, FaGithub, FaTwitter, FaInstagram } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaCloud  } from "react-icons/fa";
 
 const sociales = [
   { icon: <FaGithub />, path: "https://github.com/Spek7ro" },
   { icon: <FaLinkedin />, path: "https://www.linkedin.com/in/cristian-alvarado-712065209/" },
+  { icon: <FaCloud  />, path: "https://www.skills.google/public_profiles/579dd235-1ecd-468b-9d63-0514088676d4" },
 ];
 
 const Social = ({ containerStyle, iconStyle }) => {
